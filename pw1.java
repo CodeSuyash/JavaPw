ca572c354c2078ae3430c7ba327ac9463d249705
unknown <suyashbaranwal2003@gmail.com>
2024-04-05 20:56:04 +0530

    first commit

---
+++ pw1.java
@@ -0,0 +1,15 @@
+class hello {
+public static void main(String []args){
+System.out.println("Hello");
+System.out.println(args[0]);
+System.out.println(args[2]);
+}
+}
+
+class hello1 {
+public static void main(String []args){
+System.out.println("Hello");
+System.out.println(args[0]);
+System.out.println(args[2]);
+}
+}
