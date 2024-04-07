b2ff919249e03b34858976e30c47da717ba27d50
CodeSuyash <suyashbaranwal2003@gmail.com>
2024-04-07 14:03:55 +0530

    pw1 modified and 1 method added

--- pw1.java
+++ pw1.java
@@ -13,3 +13,11 @@ System.out.println(args[0]);
 System.out.println(args[2]);
 }
 }
+
+class hello1 {
+public static void main(String []args){
+System.out.println("Hello");
+System.out.println(args[0]);
+System.out.println(args[2]);
+}
+}
